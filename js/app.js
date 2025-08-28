c39fe570cea23151de3ad41511960a3810b587f0
Thiago Dupim Aguilar <thiagodupim3@gmail.com>
2025-08-28 16:04:30 -0300

    console.log e document.getElementById

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-window.alert("Olá mundo!")
+// window.alert("Olá mundo!")
+
+window.console.log(
+    window.document.getElementById("titulo").innerHTML // Pega o elemento pelo id e o innerHTML é para ocultar o HTML e exibir somente o conteúdo
+)
