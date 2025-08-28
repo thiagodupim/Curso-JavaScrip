4ca8ad409e471bc29a95b4eb5e5a5f82a68025eb
Thiago Dupim Aguilar <thiagodupim3@gmail.com>
2025-08-28 17:49:17 -0300

    Sobre o BOM #2

--- js/app.js
+++ js/app.js
@@ -3,5 +3,45 @@ console.log(
 )
 
 console.log(
+    screen.height // Retorna a altura
+)
+
+console.log(
+    screen.orientation.type
+)
+
+console.log(
+    navigator // Informações do navegador
+)
+
+console.log(
+    navigator.appName
+)
+
+console.log(
+    navigator.appVersion
+)
+
+console.log(
+    location
+)
+
+console.log(
+    history
+)
+    
+console.log(
     window
 )
+
+alert(screen.width)
+
+document.getElementById("largura").innerHTML = screen.width
+
+function voltar() {
+    history.back()
+}
+
+function adiantar() {
+    history.forward()
+}
