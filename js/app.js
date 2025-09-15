11889589a7fe67aa0cdb3b38ff53ee259d5eed7f
Thiago Dupim Aguilar <thiagodupim3@gmail.com>
2025-09-15 17:16:31 -0300

    DOM | getAttribute, setAttribute e removeAttribute

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
-// Retorna o elemento pai
-console.log(
-    document.getElementById("diamantina").parentNode
-)
+// Seta um atributo
+document.getElementById("btn-set").addEventListener("click", function(){
+    document.getElementById("title").setAttribute("class", "red")
+})
 
-console.log(
-    document.getElementById("diamantina").parentNode.parentNode
-)
+// Remove o atributo
+document.getElementById("btn-remove").addEventListener("click", function(){
+    document.getElementById("title").removeAttribute("class")
+})
+
+// Pega um atributo e atribui ele a um elemento
+document.getElementById("btn-get").addEventListener("click", function(){
+    var value = document.getElementById("title").getAttribute("class")
+    document.getElementById("class").innerHTML = value
+})
