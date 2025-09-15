ae75b158da442aa58d7a93b656e7d35b6cbe756a
Thiago Dupim Aguilar <thiagodupim3@gmail.com>
2025-09-15 17:47:02 -0300

    DOM | createTextNode e textContent

--- js/app.js
+++ js/app.js
@@ -1,15 +1,8 @@
-// Seta um atributo
-document.getElementById("btn-set").addEventListener("click", function(){
-    document.getElementById("title").setAttribute("class", "red")
-})
+// Inserindo um texto
+var titulo = document.querySelector("h1")
+var texto = document.createTextNode("Um texto qualquer")
 
-// Remove o atributo
-document.getElementById("btn-remove").addEventListener("click", function(){
-    document.getElementById("title").removeAttribute("class")
-})
+titulo.appendChild(texto)
 
-// Pega um atributo e atribui ele a um elemento
-document.getElementById("btn-get").addEventListener("click", function(){
-    var value = document.getElementById("title").getAttribute("class")
-    document.getElementById("class").innerHTML = value
-})
+// Desta maneira também conseguimos
+titulo.textContent = "Um novo texto"
