19bf7b1dad057ce231ca2e06fb520016198c2087
Thiago Dupim Aguilar <thiagodupim3@gmail.com>
2025-09-15 16:22:24 -0300

    DOM | parentNode

--- js/app.js
+++ js/app.js
@@ -1,19 +1,8 @@
+// Retorna o elemento pai
 console.log(
-    // Retorna os nós filhos de um nó (body no caso).
-    document.body.childNodes
+    document.getElementById("diamantina").parentNode
 )
 
-var lista = document.body.childNodes
-alert(lista.length)
-alert(lista[5].innerHTML) 
-
-// Retorna o nome do nó.
-alert(lista[5].nodeName)
-
-// Retorna o tipo do nó. Sempre irá retorna 1, 2, 3 ou 8.
-alert(lista[1].nodeType)
-
-alert(lista[1].nodeValue)
-
-// Retorna os nós de um outro nó
-alert(lista[5].childNodes[3].childNodes[0].nodeValue)
+console.log(
+    document.getElementById("diamantina").parentNode.parentNode
+)
