732a3f13a15ab3472b578bea0774d77ec75f4983
Thiago Dupim Aguilar <thiagodupim3@gmail.com>
2025-09-18 11:09:40 -0300

    Operadores lógicos, comparação e ternário

--- js/app.js
+++ js/app.js
@@ -1,23 +1,40 @@
-/* Operadores
-    Aritméticos
-    Atribuição
+/* Operadores: 
+    Lógicos
+    Comparação
+    Operador ternário
 */
 
 let x = 5
-let y = 2
-let j = 10
 
-// x = x + y
-// x -= y
-// x *= y
-// x/= y
-// x **= y
-// x %= y
-x += y
+let media = 8
+let frequencia = 80
 
-// Incremento e decremento
-j++
-// j--
+let idade = 16
 
-console.log(x)
-console.log(j)
+// Operador ternário
+let eleitor = (idade >= 16) ? "Pode votar" : "Não pode votar"
+let eleitorNot = (!idade <= 15) ? "Pode votar" : "Não pode votar"
+
+// Verificando se x é igual a 5
+console.log(x == 5)
+console.log(x != 4)
+console.log(x != "5")
+
+// Verificando o tipo de dado
+console.log(x === "5")
+console.log(x !== "5")
+
+// Maior que... Menor que
+console.log(x > 2)
+console.log(x < 2)
+
+// Maior ou igual... menor ou igual
+console.log(x >= 5)
+console.log(x <= 2)
+
+// Operadores lógicos - média
+console.log(media >= 7 && frequencia >= 70)
+console.log(media >= 7 || frequencia >= 50)
+
+console.log(eleitor)
+console.log(eleitorNot)
