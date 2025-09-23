c591c51bef8a70fbfa5a24edd843055b60c12607
Thiago Dupim Aguilar <thiagodupim3@gmail.com>
2025-09-23 09:30:57 -0300

    Métodos alert, prompt e confirm

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
 /* 
-   Functions
+   Alert = Método do window que recebe apenas um parametro (string)
+   Prompt = Método do window que recebe um parametro (string) e retorna um valor da caixa
+   Confirm = Método do window que retorna true ou false
 */
 
-function exibirAlert() {
-    alert("Funcionou!")
+alert("Isso é um alert")
+
+let nome = prompt("Digite seu nome")
+console.log(nome)
+
+let resposta = confirm("Tem certeza que deseja excluir esse item?")
+
+if (resposta == true) {
+    alert("Item excluido com sucesso!")
+} else {
+    alert("Exclusão cancelada!")
 }
-
-exibirAlert()
-
-function soma(x, y) {
-    return x + y
-}
-
-// console.log(soma(4, 6))
-
-document.getElementById("resultado").innerHTML = soma(7, 5)
